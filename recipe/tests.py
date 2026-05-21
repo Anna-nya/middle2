8671ccc374bca4d637770a16e6690aef1e966a73
Anna-nya <annarusavska1@gmail.com>
2026-05-21 15:55:05 +0300

    Add test for Recipe model creation

--- recipe/tests.py
+++ recipe/tests.py
@@ -8,3 +8,16 @@ class CategoryModelTest(TestCase):
         self.assertEqual(category.name, "Desserts")
         # Тестуємо наш метод __iter__
         self.assertEqual(dict(category), {'name': 'Desserts'})
+
+class RecipeModelTest(TestCase):
+    def test_recipe_creation(self):
+        category = Category.objects.create(name="Main Course")
+        recipe = Recipe.objects.create(
+            title="Pasta",
+            description="Delicious pasta",
+            instructions="Boil water...",
+            ingredients="Pasta, salt, water",
+            category=category
+        )
+        self.assertEqual(recipe.title, "Pasta")
+        self.assertEqual(recipe.category.name, "Main Course")
