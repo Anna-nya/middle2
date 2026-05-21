73747ee6c3020cc624fb9b528e80d2959f92f9c3
Anna-nya <annarusavska1@gmail.com>
2026-05-21 15:46:23 +0300

    Add test for Category model creation and __iter__ method

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Category, Recipe
 # Create your tests here.
+
+class CategoryModelTest(TestCase):
+    def test_category_creation(self):
+        category = Category.objects.create(name="Desserts")
+        self.assertEqual(category.name, "Desserts")
+        # Тестуємо наш метод __iter__
+        self.assertEqual(dict(category), {'name': 'Desserts'})
